c3b7550531f15d401440fcf2ffd74703cb35c8a7
Сергей <mak.sergei@ngs.ru>
2024-03-22 23:45:47 +0700

    1 commit, создание проекта

---
+++ src/main/java/com/skypro/animalShelterInfoBot/AnimalShelterInfoBotApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.animalShelterInfoBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalShelterInfoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalShelterInfoBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/animalShelterInfoBot/AnimalShelterInfoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.animalShelterInfoBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterInfoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
