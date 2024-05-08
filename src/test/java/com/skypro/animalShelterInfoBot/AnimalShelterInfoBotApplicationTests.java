f329ab5f82bbd97c849e74a21225c66c06d49f09
19kuz <19kuzminykh78@mail.ru>
2024-05-08 11:22:57 +0800

    AnimalShelterInfoBotApplication покрыт тестами.

--- src/test/java/com/skypro/animalShelterInfoBot/AnimalShelterInfoBotApplicationTests.java
+++ src/test/java/com/skypro/animalShelterInfoBot/AnimalShelterInfoBotApplicationTests.java
@@ -2,12 +2,18 @@ package com.skypro.animalShelterInfoBot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @SpringBootTest
 class AnimalShelterInfoBotApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	void mainTest() throws TelegramApiException {
+		String[] args = {"arg1", "arg2"};
+		AnimalShelterInfoBotApplication.main(args);
 	}
 
 }
